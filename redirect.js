d420f902354bac560a6db7aa6f648395e5f856a5
johnathanperez1928 <johnathanperez1928@gmail.com>
2025-09-24 22:43:46 -0500

    Add files via upload

---
+++ redirect.js
@@ -0,0 +1,13 @@
+
+document.querySelector(".bounce-house").addEventListener("click", function() {
+    window.location.href = "selection.html";
+  });
+
+  
+document.querySelector(".tent").addEventListener("click", function() {
+    window.location.href = "tents.html";
+  });
+
+  document.querySelector(".bull").addEventListener("click", function() {
+    window.location.href = "bull.html";
+  });
